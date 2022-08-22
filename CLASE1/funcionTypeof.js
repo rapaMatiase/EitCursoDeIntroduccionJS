3ae7603eb54b6bea456fb0ae30e76e109d6ce1b7
rapaMatiase <rapa.matias.e@gmail.com>
2022-08-22 15:40:33 -0300

    Revert "Clase 1 - Se volvio a dictar por inconveniente tecnico"
    This reverts commit df8f52cf4d3b87be8cae1fe54f0461ad77d2f28d.

--- CLASE1/funcionTypeof.js
+++
@@ -1,9 +0,0 @@
-/* TYPEOF es una variable que me devuelve el tipo de una variable. */
-
-let variableString = "hola"
-let variableNumerica = 2312
-let tipoDeLaVariable = typeof(variableString)
-console.log(tipoDeLaVariable)
-
-tipoDeLaVariable = typeof(variableNumerica)
-console.log(tipoDeLaVariable)
--- CLASE1/variable.js
+++
@@ -1,6 +0,0 @@
-//COMENTARIO DE UNA LIENA
-//Se utiliza la palabra reservada LET para declarar variables.
-//El igual (operacion de asignacion) se utiliza para decir que el valor de la derecha se guarda en la variable de la izquierda.
-let nombre = "un valor"
-//Por consola mostrame el valor guardado en la variable nombre
-console.log(nombre)
